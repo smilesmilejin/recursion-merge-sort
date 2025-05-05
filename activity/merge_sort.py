43b97514ba059a3f0a799f08edc9cfee5135802d
Xinshuang Jin <jinxinshuang@gmail.com>
2025-05-05 15:53:18 -0400

    implemented merge_sort

--- activity/merge_sort.py
+++ activity/merge_sort.py
@@ -5,5 +5,36 @@ from .mergelib import merge_sorted
 # already sorted lists in linear time and space
 # with respect to the combined lengths of the lists.
 
+# Time Complexisty O (n log n)
+# n elements, log n layers
+# n times to merge two list
 def merge_sort(data):
-    pass
+    # If the list is 0 or 1 long, it's trivially sorted.
+    # Make a copy in case this was the original input
+    # (We don't want to return a reference to the input data.
+    # The returned list should always be a new list.)
+    if len(data) <= 1:
+        return list(data)
+
+    # Split the array into two halves
+    mid = len(data) // 2
+    left = data[0:mid]
+    right = data[mid:]
+
+    sorted_left = merge_sort(left)  # Merge sort the front half
+    sorted_right = merge_sort(right)  # Merge sort the rear half
+
+    # Combine the two sorted halves
+    sorted_data = merge_sorted(sorted_left, sorted_right)
+
+    # Return the now sorted array
+    return sorted_data
+
+# The same code as above, but condensed
+    
+# def merge_sort(data):
+#     if len(data) <= 1:
+#         return list(data)
+    
+#     mid = len(data) // 2
+#     return merge_sorted(merge_sort(data[0:mid]), merge_sort(data[mid:]))
